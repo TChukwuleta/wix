808d08245819ae9c5e6b6aa3d876831894f673e2
Nisaba <34550856+Nisaba@users.noreply.github.com>
2024-08-05 16:29:44 +0000

    Update BTCPay.js

--- src/backend/___spi___/payment-provider/BTCPay/BTCPay.js
+++ src/backend/___spi___/payment-provider/BTCPay/BTCPay.js
@@ -109,14 +109,14 @@ export const refundTransaction = async (options, context) => {
 		customAmount: parseInt(options.refundAmount) / Math.pow(10, currencies[currency]) 
 	}
 
-	const objTest = {
+	/*const objTest = {
 		name: "Wix Refund " + options.wixRefundId,
 		description: options.reason,
 		refundVariant: 4,
 		customCurrency: currency,
 		customAmount: parseInt(options.refundAmount) / Math.pow(10, currencies[currency]),
 		url: sUrl + "api/v1/stores/" +  options.merchantCredentials.storeId + "/invoices/" + options.pluginTransactionId + "/refund"
-	}
+	}*/
 
     const response = await fetch(sUrl + "api/v1/stores/" +  options.merchantCredentials.storeId + "/invoices/" + options.pluginTransactionId + "/refund", {
         method: 'post',
@@ -132,7 +132,7 @@ export const refundTransaction = async (options, context) => {
         headers: {
             "Content-Type": "application/json; charset=utf-8"
         },
-        body: JSON.stringify(objTest)
+        body: JSON.stringify(refund)
     });
 	
 	return {
